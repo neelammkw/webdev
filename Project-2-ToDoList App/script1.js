fe7852c3242cc40573be775cc7c169bbad9c5c21
Neelam Makwana <142253620+neelammkw@users.noreply.github.com>
2023-12-15 20:50:57 +0530

    Add files via upload

---
+++ Project-2-ToDoList App/script1.js
@@ -0,0 +1,100 @@
+const addButton = document.querySelector("#add");
+const notesContainer = document.createElement("div");
+notesContainer.classList.add("notes-container");
+document.body.appendChild(notesContainer);
+
+let notesInCurrentRow = 0;
+
+
+const updateLSData = () => {
+  const textAreaData = document.querySelectorAll("textarea");
+  const notes = Array.from(textAreaData).map((note) => note.value);
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+const deleteNote = (deletedNote, container) => {
+  const storedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+  const updatedNotes = storedNotes.filter((note) => note !== deletedNote);
+  localStorage.setItem("notes", JSON.stringify(updatedNotes));
+
+  // Remove the entire container
+  container.remove();
+};
+
+const addNewNote = (text = "") => {
+    // const noteContainer = document.createElement("div");
+    // noteContainer.classList.add("note-container");
+  //   if (notesInCurrentRow === 4 || notesInCurrentRow === 0) {
+  //   createNewRow(); // Create a new row
+  // }
+
+
+  const note = document.createElement("div");
+  note.classList.add("note");
+  const htmlData = `<div class="operation">
+            <button class="edit"><i class="fa-solid  fa-edit fa-pen-to-square fa-xl"></i></button>
+            <button class="delete"><i class="fa-solid fa-trash fa-xl"></i></button>
+        </div>
+        <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea class="textarea ${text ? "hidden" : ""}">${text}</textarea>
+    </div> `;
+  note.insertAdjacentHTML("afterbegin", htmlData);
+
+  const editButton = note.querySelector(".edit");
+  const delButton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textArea = note.querySelector(".textarea");
+
+  delButton.addEventListener("click", () => {
+    const deletedNote = textArea.value;
+    deleteNote(deletedNote, note);
+    updateLSData();
+  });
+
+  textArea.value = text;
+  mainDiv.innerHTML = text;
+
+  editButton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  textArea.addEventListener("change", () => {
+    const value = textArea.value;
+    mainDiv.innerHTML = value;
+    updateLSData();
+  });
+  notesContainer.appendChild(note);
+
+  // Increment the counter
+
+  // Check if the maximum number of notes in a row is reached
+  if (notesInCurrentRow === 4) {
+    // Create a new row and reset the counter
+    const noteRow = document.createElement("div");
+    noteRow.classList.add("note-row");
+    notesContainer.appendChild(noteRow);
+    notesInCurrentRow = 0;
+  }
+  notesInCurrentRow++;
+
+//   document.body.appendChild(noteContainer);
+
+  document.addEventListener("click", (event) => {
+    if (!note.contains(event.target)) {
+      mainDiv.classList.remove("hidden");
+      textArea.classList.add("hidden");
+    }
+  });
+};
+
+// getting data back from local storage
+const notes = JSON.parse(localStorage.getItem("notes"));
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+
+addButton.addEventListener("click", () => {
+  addNewNote();
+});
